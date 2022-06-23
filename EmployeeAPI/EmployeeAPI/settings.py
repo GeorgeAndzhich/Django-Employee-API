a0e1e5201edf09ecc94b52e2d1fee75b1630b890
George Andjic <djordjeandjic99@gmail.com>
2022-06-24 00:09:51 +0300

    Created employee app

--- EmployeeAPI/EmployeeAPI/settings.py
+++ EmployeeAPI/EmployeeAPI/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'employees'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'EmployeeAPI.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.employeeDB',
     }
 }
 
